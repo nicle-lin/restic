c2715fbf2f9c13556b46ef09aa84836c9f014656
nicle-lin <dghpgyss@163.com>
2018-02-22 20:45:15 +0800

    beego

--- web/controllers/base.go
+++ web/controllers/base.go
@@ -42,22 +42,3 @@ func(c *BaseController)Finish(){
 }
 
 
-type ErrorController struct {
-	beego.Controller
-}
-
-func (c *ErrorController) Error404() {
-	c.Data["content"] = "page not found"
-	c.TplName = "error/404.html"
-}
-
-func (c *ErrorController) Error501() {
-	c.Data["content"] = "server error"
-	c.TplName = "error/501.html"
-}
-
-
-func (c *ErrorController) ErrorDatabase() {
-	c.Data["content"] = "database is now down"
-	c.TplName = "error/database.html"
-}
